d5f4ed63383c16d2a4d163199f6687b8ee039ac9
Daniel Ale <danielale9291@gmail.com>
2022-01-09 08:32:53 +0100

    Create product api base structure

---
+++ src/Services/Mango.Services.ProductApi/DbContexts/ProductApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ProductApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductApi.DbContexts;
+
+public class ProductApiDbContext : DbContext
+{
+    public ProductApiDbContext(DbContextOptions<ProductApiDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ src/Services/Mango.Services.ProductApi/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductApi.Models;
+
+public class Product
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Required(ErrorMessage = "Name is required.")]
+    public string? Name { get; set; }
+    [Range(1,1000)]
+    public double Price { get; set; }
+    public string? Description { get; set; }
+    public string? CategoryName { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
